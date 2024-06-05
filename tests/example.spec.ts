2ea0a74fdc2cfede4b72ab7da70027f071e5b264
Brian Blosser <brian.blosser@davidson.group>
2024-06-05 15:02:46 -0500

    Add test coverage.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,17 @@
 import {test, expect} from '@playwright/test';
+import MCR from "monocart-coverage-reports";
 
 test('loads initial screen', async ({page}) => {
+    await Promise.all([
+        page.coverage.startJSCoverage({
+            resetOnNavigation: false
+        }),
+        page.coverage.startCSSCoverage({
+            resetOnNavigation: false
+        })
+    ]);
+
+    // Start test body
     await page.goto('/');
 
     await expect(page.getByTestId('details')).toHaveText('Select a ship from the list to see details');
@@ -20,4 +31,19 @@ test('loads initial screen', async ({page}) => {
         "Frigate",
         "Scout Ship",
     ])
+    // End test body
+
+    const [jsCoverage, cssCoverage] = await Promise.all([
+        page.coverage.stopJSCoverage(),
+        page.coverage.stopCSSCoverage()
+    ]);
+    const coverageData = [... jsCoverage, ... cssCoverage];
+
+    const coverageReport = MCR({
+        name: 'My Coverage Report',
+        outputDir: './coverage-reports',
+        reports: ['v8', 'console-details']
+    });
+    await coverageReport.add(coverageData);
+    await coverageReport.generate();
 });
